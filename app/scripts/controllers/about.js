1c8f1352cf3f3d46142b9b0accecb8c624757028
Chris Braddock <braddock.chris@gmail.com>
2014-10-01 20:48:21 -0400

    initial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPhonegapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularPhonegapApp
+ */
+angular.module('angularPhonegapApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
